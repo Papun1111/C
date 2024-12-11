4cedd20835824fc6485c1911f1a82b06667e190f
Papun1111 <gohanmohapatra@gmail.com>
2024-12-11 22:53:07 +0530

    added till 7

--- C assignment 5/ques3.c
+++ C assignment 5/ques3.c
@@ -7,3 +7,8 @@ int a[4][5]={
 {16,17,18,19,20}};
 printf("%d\n",*(*(a+**a+2)+3));
 return 0;}
+// **a is the dereferencing of the pointer to the first element of the array. Since a is equivalent to &a[0][0], dereferencing it once (*a) gives the address of the first row (a[0]), and dereferencing it again (**a) gives the value of the first element of the first row, which is 1.
+// a + **a + 2 translates to a + 1 + 2 because **a is 1. This expression hence simplifies to a + 3, which is the address of the fourth row (a[3]).
+// *(a + 3) is equivalent to a[3], which is the pointer to the fourth row of the array.
+// Finally, *(a + 3) + 3 points to the fourth element of the fourth row, which is 19. Thus, *(*(a + 3) + 3) dereferences to the value 19
+//ans =19
