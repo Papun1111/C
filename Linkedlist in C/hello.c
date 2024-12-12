e66249a9649fba82030ed0b2ff04eaa24668377b
Papun1111 <gohanmohapatra@gmail.com>
2024-12-12 16:20:40 +0530

    ll added

--- Linkedlist in C/hello.c
+++ Linkedlist in C/hello.c
@@ -1,75 +1,120 @@
 #include<stdio.h>
 #include<stdlib.h>
-
 struct Node
 {
     int data;
-    struct Node* next;
+   struct Node* next;
 };
-static struct Node* head=NULL;
+ static struct Node* head=NULL;
 void add(int val){
-if(head==NULL){
+ if(head==NULL){
     head=malloc(sizeof(head));
-    head->data=val;
+head->data=val;
     head->next=NULL;
-    return;
+    return ;
 }
 struct Node* newNode=malloc(sizeof(head));
-struct Node* temp=head;
 newNode->data=val;
 newNode->next=NULL;
+struct Node* temp=head;
 while (temp->next!=NULL)
 {
     temp=temp->next;
 }
 temp->next=newNode;
-}
-void addFirst(int val){
-    if(head==NULL){
-    head=malloc(sizeof(head));
-    head->data=val;
-    head->next=NULL;
-    return;
-}
-struct Node* newNode=malloc(sizeof(head));
-newNode->data=val;
-newNode->next=head;
-head=newNode;
-}
-void delete(){
-struct Node* temp=head;
-struct Node* second;
-while (temp->next!=NULL)
-{
-    second=temp;
-    temp=temp->next;
-}
-second->next=NULL;
-free(temp);
-temp=NULL;
+newNode->next=NULL;
 }
 void print(){
     struct Node* temp=head;
-    while ((temp!=NULL))
+    while (temp!=NULL)
     {
         printf("%d ",temp->data);
         temp=temp->next;
     }
     
 }
-void deleteFirst(){
- struct Node* temp=head;
-    head=head->next;
+void addFirst(int val){
+    if(head==NULL){
+    head=malloc(sizeof(head));
+head->data=val;
+    head->next=NULL;
+    return ;
+}
+struct Node* newNode=malloc(sizeof(head));
+newNode->next=head;
+newNode->data=val;
+head=newNode;
+}
+void delete(){
+ if(head==NULL){
+        printf("Bhai kuch hai hee nahi");
+        return;
+    }
+    if (head->next==NULL)
+    {
+        head=NULL;
+        return;
+    }
+    struct Node* temp=head;
+    struct Node* curr=head;
+    while(temp->next!=NULL){
+    curr=temp;
+    temp=temp->next;
+    }
+    curr->next=NULL;
     free(temp);
     temp=NULL;
 }
-int main(){
-
-add(45);
-add(23);
-add(45);
-addFirst(25);
-delete();
-deleteFirst();
-print();
+void deleteFirst(){
+    if(head==NULL){
+        printf("Bhai kuch hai hee nahi");
+        return;
+    }
+    if (head->next==NULL)
+    {
+        head=NULL;
+        return;
+    }
+    struct Node* newNode=malloc(sizeof(head));
+    newNode=head;
+    head=head->next;
+    free(newNode);
+    newNode=NULL;
 }
+void insatidex(int val,int idx){
+struct Node* temp=head;
+for(int i=0;i<idx-1;i++){
+temp=temp->next;
+}
+struct Node* newnode=malloc(sizeof(head));
+newnode->data=val;
+newnode->next=temp->next->next;
+temp->next=newnode;
+}
+void delatidex(int idx){
+if(head==NULL){
+        printf("Bhai kuch hai hee nahi");
+        return;
+    }
+    if (head->next==NULL)
+    {
+        head=NULL;
+        return;
+    }
+struct Node* temp=head;
+for(int i=0;i<idx-1;i++){
+temp=temp->next;
+}
+temp->next=temp->next->next;
+}
+ int main(){
+add(32);
+add(34);
+add(345);
+addFirst(23);
+
+delatidex(2);
+insatidex(34,1);
+print();
+    return 0;
+ }
